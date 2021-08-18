18b5ba8c49136d6af99afde5db7715e2a6c22ee9
rangerblake15 <31942298+rangerblake15@users.noreply.github.com>
2021-08-18 10:53:08 -0500

    Update SIRT-Browser.ps1
    final hail mary before pulling to other tickets

--- SIRT-Browser.ps1
+++ SIRT-Browser.ps1
@@ -1,10 +1,9 @@
-$ErrorActionPreference='silentlycontinue'
 Get-Process iexplore -ErrorAction SilentlyContinue | Stop-Process -Force -ErrorAction SilentlyContinue -Verbose
 $tempInternetFolders = @( "C:\Users\$env:username\Appdata\Local\Temp\Microsoft\Windows\Temporary Internet Files\*", 
                           "C:\Users\$env:username\Appdata\Local\TMicrosoft\Windows\INetCache\*", 
                           "C:\Users\$env:username\Appdata\Local\Microsoft\Windows\Cookies\*"
                         )
-Get-ChildItem -Path $tempInternetFolders -Recurse | Remove-Item
+Get-ChildItem -Path $tempInternetFolders -Recurse | Remove-Item -force
 $t_path_7 = "C:\Users\$env:username\AppData\Local\Microsoft\Windows\Temporary Internet Files"
 $c_path_7 = "C:\Users\$env:username\AppData\Local\Microsoft\Windows\Caches"
 $temporary_path =  Test-Path $t_path_7
@@ -14,10 +13,10 @@ if($temporary_path -eq $True -And $check_cache -eq $True)
     #Delete History
     RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 1
     RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 8
-    Get-ChildItem -Path $t_path_7 -Recurse | Remove-Item
+    Get-ChildItem -Path $t_path_7 -Recurse | Remove-Item -force
     RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 2
     #Delete Cache
-    Get-ChildItem -Path $c_path_7 -Recurse | Remove-Item
+    Get-ChildItem -Path $c_path_7 -Recurse | Remove-Item -force
 }   
 Get-Process chrome -ErrorAction SilentlyContinue | Stop-Process -Force -Verbose -ErrorAction SilentlyContinue
 $Items = @('Archived History',
@@ -31,7 +30,7 @@ $Items = @('Archived History',
 $Folder = "$($env:LOCALAPPDATA)\Google\Chrome\User Data\Default"
 $Items | % { 
     if (Test-Path "$Folder\$_") {
-        Remove-Item "$Folder\$_" 
+        Get-ChildItem -Path "$Folder\$_" -Recurse | Remove-Item -force
     }
 }
 Get-Process firefox -ErrorAction SilentlyContinue | Stop-Process -ErrorAction SilentlyContinue -Force -Verbose
@@ -39,13 +38,13 @@ $LocalDataDir = "C:\Users\$($env:USERNAME)\AppData\Local\Mozilla\Firefox\Profile
 $RoamingDataDir = "C:\Users\$($env:USERNAME)\AppData\Roaming\Mozilla\Firefox\Profiles\*"
 if (ls $LocalDataDir -ErrorAction SilentlyContinue)
 {
-    Get-ChildItem -Path $LocalDataDir -Recurse | Remove-Item
+    Get-ChildItem -Path $LocalDataDir -Recurse | Remove-Item -force
 }
 if (ls $RoamingDataDir)
 {
     $sqlTables = [System.String]::Concat($file.FullName,"\*sqlite")
     foreach ($table in $(ls $sqlTables))
     {   
-        Get-ChildItem -Path $table -Recurse | Remove-Item
+        Get-ChildItem -Path $table -Recurse | Remove-Item -force
     }
 }
